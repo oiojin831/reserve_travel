5e9a29f5cdfbc8c6c62c3dc69e3c54937c24ed33
Eung Jin Lee <oiojin831@gmail.com>
2016-09-06 01:56:32 +0900

    fix bug

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   root 'time_slots#index'
   resources :time_slots
   get '/keyboard', to: 'webhooks#keyboard'
-  get '/message', to: 'webhooks#message'
+  post '/message', to: 'webhooks#message'
 end
