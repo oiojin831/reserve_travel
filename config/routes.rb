8151f953ded2ff1620ee1b944f30763de4f28187
Eung Jin Lee <oiojin831@gmail.com>
2016-09-06 01:52:41 +0900

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'time_slots#index'
   resources :time_slots
-  get '/keyboard', to: 'keyboards#callback'
+  get '/keyboard', to: 'webhooks#keyboard'
+  get '/message', to: 'webhooks#message'
 end
